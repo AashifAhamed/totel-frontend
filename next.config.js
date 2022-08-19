1b9384c54079c01f180de18d43b5bc8a2577fd98
Ryel Banfield <ryelbanfield@gmail.com>
2022-08-19 17:58:47 -0400

    Create payment module

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['github.com', 'res.cloudinary.com'],
   },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 }
 
 module.exports = nextConfig
